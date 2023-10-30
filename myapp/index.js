9f9f4c450ab185b7cd989385b1187e457389b03d
Ogodo Sylvester <sylvestcompany@gmail.com>
2023-10-30 18:59:12 +0100

    first commit

---
+++ myapp/index.js
@@ -0,0 +1,8 @@
+let open = document.getElementById("mobile_view")
+function openNav(){
+    
+    open.classList.toggle("open_navtoggle")
+}
+function closeNav(){
+    open.classList.remove("open_navtoggle")
+}
